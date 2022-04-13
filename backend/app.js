285b49d9441d56805b367f56a2cfe75011772f22
Thomas Milton <perso.milton@gmail.com>
2022-04-13 15:14:35 +0200

    ajout backend API

---
+++ backend/app.js
@@ -0,0 +1,72 @@
+const env = require('./env')
+const express = require('express')
+const Thing = require('./models/Thing')
+
+const app = express()
+
+const mongoose = require('mongoose')
+mongoose
+  .connect(env.MONGODB_API_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+app.use(express.json())
+
+// ajout des middleswares
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id
+
+  const thing = new Thing({
+    ...req.body,
+  })
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch((error) => res.status(400).json({ error }))
+})
+
+// Récupère un seul objet par l'ID
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }))
+})
+
+// Modifie l'objet
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch((error) => res.status(400).json({ error }))
+})
+
+// Supprimer l'objet
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch((error) => res.status(400).json({ error }))
+})
+
+// Récupère tout les objets
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }))
+})
+
+module.exports = app
